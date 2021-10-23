40ea51e53263775b125daacd3037b8bf049c675d
ajijshaikh <35911904+ajijshaikh@users.noreply.github.com>
2021-10-23 23:05:38 +0530

    Updated naming conventions

--- Bank.java
+++ Bank.java
@@ -4,43 +4,44 @@ public class Bank {
 
     int AccountNumber;
     int Balance;
-    String AccountName;
 
-    public Bank(String AccountName, int AccountNumber, int Balance) {
+    public Bank(int AccountNumber, int Balance) {
         this.AccountNumber = AccountNumber;
         this.Balance = Balance;
-        this.AccountName = AccountName;
     }
 
-    public void fundTransfer(Bank OtherAccount, int Amount){
-        ensureSufficientFundsAreAvailable(this);
-        this.debitFromAccount(Amount);
-        OtherAccount.creditToAccount(Amount);
+    public void fundTransfer(Bank DestinationAccount, int Amount){
+        ensureSufficientFundsAreAvailable(this, Amount);
 
-        System.out.println("First account balance : " + this.Balance);
-        System.out.println("Second account balance : " + OtherAccount.Balance);
+        this.debit(Amount);
+        DestinationAccount.credit(Amount);
+
+        System.out.println("Source account balance : " + this.Balance);
+        System.out.println("Destination account balance : " + DestinationAccount.Balance);
     }
 
-    private void debitFromAccount(int Amount) {
+    private void debit(int Amount) {
         this.Balance -= Amount;
     }
 
-    private void creditToAccount(int Amount){
+    private void credit(int Amount){
         this.Balance += Amount;
     }
 
-    private void ensureSufficientFundsAreAvailable(Bank Account){
-        if(Account.Balance <= 0){
-            throw new RuntimeException("Oops Your " + Account.AccountName +" has no money please credit some amount to it. ");
+    private void ensureSufficientFundsAreAvailable(Bank SourceAccount, int amount){
+        if(SourceAccount.Balance <= 0){
+            throw new RuntimeException("Oops Your source account has no money please credit some amount to it. ");
+        }else if(SourceAccount.Balance < amount){
+            throw new RuntimeException("Sorry ! your source account wont have " + amount + " RS please retry with lesser amount than that");
         }
     }
 
 
     public static void main(String[] args) {
 
-        Bank FirstAccount = new Bank("FirstAccount",12345,700);
-        Bank SecondAccount = new Bank("SecondAccount",54321,300);
+        Bank SourceAccount = new Bank(12345,700);
+        Bank DestinationAccount = new Bank(54321,300);
 
-        FirstAccount.fundTransfer(SecondAccount,100);
+        SourceAccount.fundTransfer(DestinationAccount,500);
     }
 }
